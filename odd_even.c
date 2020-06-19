b907b2b19ce52f69638945754bfdd6c89ea5b12f
Shilpa Kumari <gshilpa690@gmail.com>
2020-06-19 18:22:03 +0530

    check even odd

---
+++ odd_even.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+void main()
+{
+	int num;
+	printf("enter an integer: ");
+	scanf("%d", &num);
+
+	if(num % 2 == 0)
+		printf("%d is an even number",num);
+	else
+		printf("%d is an odd number",num);
+}
